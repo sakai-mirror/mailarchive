2e5cc59da0a059573641b88d17d5944c3c07f7c5
david.horwitz@uct.ac.za <david.horwitz@uct.ac.za@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2008-02-11 09:00:31 +0000

    svn log -r41081 https://source.sakaiproject.org/svn/mailarchive/trunk/ ------------------------------------------------------------------------ r41081 | bkirschn@umich.edu | 2008-02-07 22:10:33 +0200 (Thu, 07 Feb 2008) | 1 line
    SAK-5849 fix ParseException error
    ------------------------------------------------------------------------
    dhorwitz@david-horwitz-6:~/branchManagemnt/sakai_2-5-x> svn merge -c41081 https://source.sakaiproject.org/svn/mailarchive/trunk/ mailarchive/
    U    mailarchive/mailarchive-james/james/src/java/org/sakaiproject/james/SakaiMailet.java
    dhorwitz@david-horwitz-6:~/branchManagemnt/sakai_2-5-x> svn log -r41083 https://source.sakaiproject.org/svn/mailarchive/trunk/
    ------------------------------------------------------------------------
    r41083 | bkirschn@umich.edu | 2008-02-08 00:48:45 +0200 (Fri, 08 Feb 2008) | 1 line
    
    SAK-5849 SAK-10285 need to decode & encode UTF8 filenames
    ------------------------------------------------------------------------
    dhorwitz@david-horwitz-6:~/branchManagemnt/sakai_2-5-x> svn merge -c41083 https://source.sakaiproject.org/svn/mailarchive/trunk/ mailarchive/
    G    mailarchive/mailarchive-james/james/src/java/org/sakaiproject/james/SakaiMailet.java
    
    
    git-svn-id: https://source.sakaiproject.org/svn/mailarchive/branches/sakai_2-5-x@41122 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- mailarchive-james/james/src/java/org/sakaiproject/james/SakaiMailet.java
+++ mailarchive-james/james/src/java/org/sakaiproject/james/SakaiMailet.java
@@ -30,6 +30,7 @@ import java.util.Enumeration;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Vector;
+import java.net.URLEncoder;
 
 import javax.mail.Address;
 import javax.mail.BodyPart;
@@ -84,6 +85,9 @@ public class SakaiMailet extends GenericMailet
 
 	/** The user name of the postmaster user - the one who posts incoming mail. */
 	public static final String POSTMASTER = "postmaster";
+	
+	// used when parsing email header parts
+	private static final String NAME_PREFIX = "name=";
 
 	// Condition: The site doesn't have an email archive turned on
 	public final String errorMsg_I = "Your message cannot be delivered because the site you are emailing"
@@ -587,8 +591,23 @@ public class SakaiMailet extends GenericMailet
 		// everything else gets treated as an attachment
 		else
 		{
-			ContentType cType = new ContentType(type);
 			String name = p.getFileName();
+			
+			// look for UTF8 encoded filenames
+			if ( name == null && type.indexOf(NAME_PREFIX) != -1 )
+			{
+				String encodedFilename = type.substring( type.indexOf(NAME_PREFIX)+NAME_PREFIX.length() );
+				name = MimeUtility.decodeText( encodedFilename );
+				type = type.replace( encodedFilename, name );
+			}
+			
+			// ContentType can't handle filenames with spaces or UTF8 characters
+			if ( name != null )
+			{
+				type = type.replace( name, URLEncoder.encode(name, "UTF-8") );
+			}
+			
+			ContentType cType = new ContentType(type);
 			String disposition = p.getDisposition();
 			int approxSize = p.getSize();
 
