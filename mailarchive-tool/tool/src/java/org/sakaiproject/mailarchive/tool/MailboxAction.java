2fd8f7f44fe7a5e13141a79a7ff3775f5917f021
david.horwitz@uct.ac.za <david.horwitz@uct.ac.za@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2009-04-10 09:04:39 +0000

    SAK-13054 remove previously reverted change accidentaly merged back in.
    > svn merge -c-59885
    > https://source.sakaiproject.org/svn/mailarchive/branches/sakai_2-6-x
    > mailarchive/
    --- Reverse-merging r59885 into 'mailarchive':
    U
    mailarchive/mailarchive-tool/tool/src/java/org/sakaiproject/mailarchive/tool/MailboxAction.java
    U    mailarchive/mailarchive-tool/tool/src/bundle/email.properties
    
    
    
    git-svn-id: https://source.sakaiproject.org/svn/mailarchive/branches/sakai_2-6-x@59927 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- mailarchive-tool/tool/src/java/org/sakaiproject/mailarchive/tool/MailboxAction.java
+++ mailarchive-tool/tool/src/java/org/sakaiproject/mailarchive/tool/MailboxAction.java
@@ -961,13 +961,7 @@ System.out.println("doRemove_confirmed id="+msgId);
 						// then add the desired alias
 						if (alias != null)
 						{
-							//check if alias = "postmaster"
-							// SAK-13054
-							if (alias.equalsIgnoreCase("postmaster")) {
-								addAlert(state, rb.getString("theemaali4"));
-							} else {
-								AliasService.setAlias(alias, channel.getReference());
-							}
+							AliasService.setAlias(alias, channel.getReference());
 						}
 					}
 					catch (Exception any)
